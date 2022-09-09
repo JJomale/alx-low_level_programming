35bef89eeb7749a9bb263f24b0d0097f8dca8881
JJomale <infojjomale2020@gmail.com>
2022-09-09 16:37:25 -0700

    executable named

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "programing is like builing a multilingusl puzzle
+ * this program prints "programming is like builing a multilingusl puzzle
  * Return: o
  */
 int main(void)
 {
-	puts("\"programing is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
