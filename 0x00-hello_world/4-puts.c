015d0c5d205bdaf04c9c10922ed438e45fc11b11
JJomale <infojjomale2020@gmail.com>
2022-09-09 16:20:17 -0700

    executable named

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *
+ * Description: using the main function
+ * this program prints "programming is like builing a multilingusl puzzle
+ * Return o
+ */
+int main(void)
+{
+	puts"\"programing is like building a multilingual puzzle");
+	return (0);
+}
