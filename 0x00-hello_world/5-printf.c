7f8a5843f01bebcb9bc431dab438cd5c9a7fa8d3
JJomale <infojjomale2020@gmail.com>
2022-09-09 16:50:34 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print string in the function
+ *
+ * description: using main function
+ * program to print "programmig is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, nut the outcome is a piece of art, \n");
+	return (0);
+}
