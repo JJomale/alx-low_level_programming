366ac3f406815f8d5d931cec4094750f3f603015
JJomale <infojjomale2020@gmail.com>
2022-09-12 18:53:42 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdion.h>
+/**
+ * main - a program to print
+ * all single digits number of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i, 10; 1++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
