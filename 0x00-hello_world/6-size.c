d31bf532142e30a86f44f749c59cd2136cc6342c
JJomale <infojjomale2020@gmail.com>
2022-09-13 12:36:54 -0700

    my frist commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 	char c;
-	int 1;
-	long int 11;
-	long long int 111;
+	int l;
+	long int ll;
+	long long int lll;
 	float f;
 
 	printf("size of a char: %lb byte(s)\n", sizeof(c));
-	printf("size of an int: %lb byte(s)\n", sizeof(1));
-	printf("size of a long int: %lb byte(s)\n", sizeof(11));
-        printf("size of a long long int: %lb byte(s)\n", sizeof(111));
+	printf("size of an int: %lb byte(s)\n", sizeof(l));
+	printf("size of a long int: %lb byte(s)\n", sizeof(ll));
+        printf("size of a long long int: %lb byte(s)\n", sizeof(lll));
         printf("size of a float: %lb byte(s)\n", sizeof(f));
 	return (0);
 }
