0f70716fcab8d6ae5f025ba7f4b37603f0d14bc8
JJomale <infojjomale2020@gmail.com>
2022-09-12 20:23:24 -0700

    my first commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print number from 0 to 10 
+ * main - print number from 0 to 10
  * with commas and spaces
  *
  * Description: using the main function
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char k;
-
+	
 	for (k = '0'; k <= '9'; k++)
 	{
 		putchar(k);
