94c59a66328feff607b0b448540b06ad7c86da8b
JJomale <infojjomale2020@gmail.com>
2022-09-12 18:41:28 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program to print alphabet in lowercase
+ * that do not include e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');#
+	return (0);
+}
