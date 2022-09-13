90b200434153520c20e6a56570913413ad09b526
JJomale <infojjomale2020@gmail.com>
2022-09-12 19:11:17 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program to print single numbers
+ * digits from 0 t0 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i =9; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
