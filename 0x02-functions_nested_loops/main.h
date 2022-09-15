22ccc2d65e69b359f9b4f1ae8619271bb6f75f48
JJomale <infojjomale2020@gmail.com>
2022-09-15 08:13:04 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H-INCLUDED
 
-int putchar(char);
+int _putchar(char);
 void print_alphabet(void);
-void print_alphabet-x10(void);
+void print_alphabet_x10(void);
 int isalower(int c);
 int isalpha(int c);
 int print-sign(int n);
-int _abs digits(int);
+int _abs(int);
 int print last_digits(int)
 void jack_bauer(void);
 void time_table(void);
