8692ce8920615d768bd192f833c359f0334a81bb
JJomale <infojjomale2020@gmail.com>
2022-09-15 08:22:26 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int isalower(int c);
-int isalpha(int c);
+int _isalower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digits(int)
