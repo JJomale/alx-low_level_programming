a6b66d95064804ca98c40e51da9536cfe9bd6b95
JJomale <infojjomale2020@gmail.com>
2022-09-15 01:23:18 -0700

    variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
  * main - program that prints all possible combinations
- * Description: using the main function
- * this program print "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ *
  * Return: 0
  */
 int main(void)
 {
 	int c;
 
-	for (c = 0; c <= 10; c++)
+	for (c = 0; c < 10; c++)
 	{
 		putchar(c + '0');
 		if (c != 9)
