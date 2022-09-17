145a9060154c0c5bcc0d6600b5d3380fbde9e822
JJomale <infojjomale2020@gmail.com>
2022-09-16 22:35:02 -0700

    functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,45 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - prints any number it's give it in ASCII
- * @n: number to be printed
- * Description: converts a number and prints
- * each digit of the number in ASCII format, no
- * matter how large the number is
+ * print_number - a function that prints an integer
+ *
+ * @n: parameter
+ * 
+ * Return: void
  */
+
 void print_number(int n)
 {
-	int isnegative = 0, temp;
-	unsigned int sigfig = 0;
-	int i, j;
+	unsigned int number;
+
+	number = n;
 
 	if (n < 0)
 	{
-		isnegative = 1;
-		n = abs(n);
-	}
-	temp = n;
-	while (temp)
-	{
-		temp = temp / 10;
-		sigfig++;
-	}
-	sigfig--;
-
-	j = sigfig;
-
-	sigfig = 1;
-	for (i = 1; i <= j; i++)
-	{
-		sigfig = sigfig * 10;
-	}
-	if (isnegative)
 		_putchar('-');
-	temp = n;
-	while (sigfig)
-	{
-		_putchar(temp / sigfig + '0');
-		temp = temp % sigfig;
-		sigfig = sigfig / 10;
+		number = -number;
 	}
+	if ((number / 10) > 0)
+	{
+		print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
 }
