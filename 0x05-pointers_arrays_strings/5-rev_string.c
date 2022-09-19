1fae0c9ef90160e0aa8c65ac9f3e9b484427f7dd
JJomale <infojjomale2020@gmail.com>
2022-09-19 15:59:26 -0700

    array point and string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * re_string - reverses a tring
+ * @s: string to be reserved
+ *
+ * Return: nothing
+ */
+void rev_string(cha *s)
+{
+	int i, tmp, len = _strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *(s + i);
+	*(s + i) = *(s + len - i - 1);
+	*(s + len - i - 1) = tmp;
+	}
+}
+/**
+ * _strlen - return the lenght of a string
+ * @s: string
+ *
+ * REtrun: the lenght of the givn string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*(s + len) != '\0')
+	len++
+
+		:return (len);
+}
