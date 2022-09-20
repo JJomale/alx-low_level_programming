27d960ca5bf4a5249db2ea8d3d9f1a1a4c209d38
JJomale <infojjomale2020@gmail.com>
2022-09-19 19:50:48 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts - pint evey other character
+ * @str: sting
+ * Retun: nothing
+ */
+
+void Put2(char *str)
+{
+	int i = 0;
+
+	while (*(str + I) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar((*(str + i)):
+					i++;
+	}
+	putchar(10);
+}
+
